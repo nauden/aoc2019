e8d7b8592bfd099e8a9df40f5f24011234efeb96
nauden <nauden@gmail.com>
2019-12-04 09:40:17 +0100

    Refactor day 1

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,27 +1,26 @@
 defmodule Day01 do
-  def fuel(mass) when mass < 6, do: 0
+  import Enum, only: [map: 2, sum: 1]
+
   def fuel(mass), do: div(mass, 3) - 2
 
   def input() do
     File.read!("input/day01.txt")
     |> String.split("\n", trim: true)
-    |> Enum.map(&String.to_integer/1)
+    |> map(&String.to_integer/1)
   end
 
   def part1 do
-    input()
-    |> Enum.map(&fuel/1)
-    |> Enum.sum()
+    input() |> map(&fuel/1) |> sum()
   end
 
   def part2 do
     input()
-    |> Enum.map(fn m ->
+    |> map(fn m ->
       fuel(m)
       |> Stream.iterate(&fuel/1)
       |> Enum.take_while(&(&1 > 0))
-      |> Enum.sum()
+      |> sum()
     end)
-    |> Enum.sum()
+    |> sum()
   end
 end
