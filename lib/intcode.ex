ea0649881e0f207add993b8e5a338d8365d3d4b9
nauden <nauden@gmail.com>
2019-12-09 16:29:02 +0100

    Modify IntCode module
    * Changed from using a tuple to erlang array
    * Added opcode 9 (adjust base offset)
    * Added relative parameter mode

--- lib/intcode.ex
+++ lib/intcode.ex
@@ -7,10 +7,12 @@ defmodule Intcode do
   @jz 6
   @lt 7
   @eq 8
+  @adjust 9
   @halt 99
 
   @position 0
   @immediate 1
+  @relative 2
 
   def decode(opcode) do
     {
@@ -26,39 +28,48 @@ defmodule Intcode do
   def pad(l, n) when length(l) >= n, do: l
   def pad(l, n), do: pad(l ++ [0], n)
 
-  def fetch(modes, ip, ins, n) do
+  def fetch(modes, cpu, n) do
     modes
     |> Enum.with_index(1)
     |> Enum.take(n)
-    |> Enum.map(fn {mode, i} -> get(mode, ip, ins, i) end)
+    |> Enum.map(fn {mode, i} -> get(mode, cpu, i) end)
   end
 
-  def get(mode, ip, ins, n) do
+  def get(mode, %{ip: ip, ins: ins, base: base}, n) do
     case mode do
-      @position -> elem(ins, elem(ins, ip + n))
-      @immediate -> elem(ins, ip + n)
+      @position -> :array.get(:array.get(ip + n, ins), ins)
+      @immediate -> :array.get(ip + n, ins)
+      @relative -> :array.get(base + :array.get(ip + n, ins), ins)
+    end
+  end
+
+  def idx(mode, %{ip: ip, ins: ins, base: base}, n) do
+    case mode do
+      @position -> :array.get(ip + n, ins)
+      @immediate -> raise "cannot get immediate index"
+      @relative -> base + :array.get(ip + n, ins)
     end
   end
 
   def binary_op(modes, %{ip: ip, ins: ins} = cpu, op) do
-    [a, b] = fetch(modes, ip, ins, 2)
-    p = elem(ins, ip + 3)
+    [a, b] = fetch(modes, cpu, 2)
+    p = idx(List.last(modes), cpu, 3)
 
-    %{cpu | :ip => ip + 4, :ins => put_elem(ins, p, op.(a, b))}
+    %{cpu | :ip => ip + 4, :ins => :array.set(p, op.(a, b), ins)}
     |> step()
   end
 
-  def input(%{ip: ip, ins: ins, input: [h | t]} = cpu) do
-    a = elem(ins, ip + 1)
+  def input(modes, %{ip: ip, ins: ins, input: [h | t]} = cpu) do
+    p = idx(hd(modes), cpu, 1)
 
-    %{cpu | :ip => ip + 2, :ins => put_elem(ins, a, h), input: t}
+    %{cpu | :ip => ip + 2, :ins => :array.set(p, h, ins), input: t}
     |> step()
   end
 
-  def output(modes, %{ip: ip, ins: ins} = cpu) do
-    [a] = fetch(modes, ip, ins, 1)
+  def output(modes, %{ip: ip, output: os} = cpu) do
+    a = fetch(modes, cpu, 1)
 
-    cpu = %{cpu | :ip => ip + 2, :output => a}
+    cpu = %{cpu | :ip => ip + 2, :output => os ++ a}
 
     if Map.has_key?(cpu, :pause) do
       {:paused, cpu}
@@ -68,49 +79,58 @@ defmodule Intcode do
   end
 
   def jmp_op(modes, cpu, cmp) do
-    [a, b] = fetch(modes, cpu.ip, cpu.ins, 2)
+    [a, b] = fetch(modes, cpu, 2)
 
     %{cpu | :ip => if(cmp.(a, 0), do: b, else: cpu.ip + 3)}
     |> step()
   end
 
   def set_if_op(modes, %{ip: ip, ins: ins} = cpu, cmp) do
-    [a, b] = fetch(modes, ip, ins, 2)
-    p = elem(ins, ip + 3)
+    [a, b] = fetch(modes, cpu, 2)
+    p = idx(List.last(modes), cpu, 3)
 
     n = if cmp.(a, b), do: 1, else: 0
 
-    %{cpu | :ip => ip + 4, :ins => put_elem(ins, p, n)}
+    %{cpu | :ip => ip + 4, :ins => :array.set(p, n, ins)}
+    |> step()
+  end
+
+  def adjust(modes, %{ip: ip, base: base} = cpu) do
+    [a] = fetch(modes, cpu, 1)
+
+    %{cpu | :ip => ip + 2, :base => base + a}
     |> step()
   end
 
   def run(cpu) when is_map(cpu), do: step(cpu)
 
-  def run(ins, input) when is_tuple(ins) do
-    %{ip: 0, ins: ins, input: [input], output: 0}
+  def run(ins, input) do
+    %{ip: 0, ins: ins, base: 0, input: [input], output: []}
     |> run()
   end
 
   def step(%{ip: ip, ins: ins} = cpu) do
-    {opcode, modes} = decode(elem(ins, ip))
+    {opcode, modes} = decode(:array.get(ip, ins))
 
     case opcode do
       @add -> binary_op(modes, cpu, &+/2)
       @mul -> binary_op(modes, cpu, &*/2)
-      @input -> input(cpu)
+      @input -> input(modes, cpu)
       @output -> output(modes, cpu)
       @jnz -> jmp_op(modes, cpu, &!=/2)
       @jz -> jmp_op(modes, cpu, &==/2)
       @lt -> set_if_op(modes, cpu, &</2)
       @eq -> set_if_op(modes, cpu, &==/2)
-      @halt -> cpu.output
+      @adjust -> adjust(modes, cpu)
+      @halt -> cpu
     end
   end
 
   def parse(string) do
     string
+    |> String.trim()
     |> String.split(",", trim: true)
     |> Enum.map(&String.to_integer/1)
-    |> List.to_tuple()
+    |> :array.from_list(0)
   end
 end
